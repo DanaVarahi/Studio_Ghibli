37b3a4bd2b18fa9081c61ac6bb2c95d0ca2e03d7
Dana Varahi <dana_varahi@protonmail.com>
2020-11-28 19:14:07 +0000

    Fixed chart not loading my adding watch function to mounted

--- src/components/SpeciesList.vue
+++ src/components/SpeciesList.vue
@@ -20,20 +20,17 @@ export default {
     watch: {
         immediate: true,
         species: function(){
-            this.speciesData = []
-            let res
-            for ( const url of this.species){
-                console.log(url)
-                fetch(url)
-                .then(res => res.json())
-                .then( data => this.processSpeciesData(data))
-            }
-
-            return this.speciesData
+           this.processUrls()
         }  
     },
 
+    mounted:function(){
+            this.processUrls()
+        },    
+    
+
     methods: {
+       
         processSpeciesData: function(data){
             const speciesName = data.name
             const totalCharacters = data.people.length
@@ -46,20 +43,34 @@ export default {
         drawChart: function() {
 
         // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Species');
-        data.addColumn('number', 'Total');
-        data.addRows( this.speciesData );
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Species');
+            data.addColumn('number', 'Total');
+            data.addRows( this.speciesData );
 
         // Set chart options
-        var options = {'title':'Species of the Characters',
-                       'width':400,
-                       'height':300};
+            var options = {'title':'Species of the Characters',
+                            'width':400,
+                            'height':300};
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        },
+
+        processUrls: function(){
+            this.speciesData = []
+            let res
+            for ( const url of this.species) {
+                console.log(url)
+                fetch(url)
+                .then(res => res.json())
+                .then( data => this.processSpeciesData(data))
+            }
+
+            return this.speciesData
+        }
+
     }
 }
 
